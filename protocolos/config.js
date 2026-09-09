757b3b2ab1384e38c4f7cb318360f897c34d80cd
PEAS - Technology <127704090+peas8810@users.noreply.github.com>
2026-09-09 16:09:28 -0300

    feat: adiciona configuração do portal de protocolos NICE

---
+++ protocolos/config.js
@@ -0,0 +1,13 @@
+window.NICE_PORTAL_CONFIG = Object.freeze({
+  // URL do Web App do Google Apps Script. Será preenchida após a publicação do backend.
+  // Exemplo: https://script.google.com/macros/s/AKfycb.../exec
+  API_URL: "",
+
+  FORM_PROTOCOLO_URL: "https://forms.gle/vQoChVX2mj61KZz27",
+  FORM_RELATORIO_URL: "https://docs.google.com/forms/d/e/1FAIpQLSei77aFurJhdDZmmrOPZRAssw5OS9NOoT5xRGK8JwRMxxSlTg/viewform?usp=sharing&ouid=117860724975449383583",
+  PAINEL_URL: "../atual/",
+
+  REFRESH_SECONDS: 60,
+  PUBLIC_TITLE: "NICE · Sistema de Protocolos",
+  INSTITUTION: "AlfaUnipac"
+});
