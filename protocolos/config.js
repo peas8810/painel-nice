71254ecda927d100ff4212bb8035eb75c2bb13da
PEAS - Technology <127704090+peas8810@users.noreply.github.com>
2026-09-09 17:16:48 -0300

    feat: conecta portal NICE ao Web App do Apps Script

--- protocolos/config.js
+++ protocolos/config.js
@@ -1,7 +1,5 @@
 window.NICE_PORTAL_CONFIG = Object.freeze({
-  // URL do Web App do Google Apps Script. Será preenchida após a publicação do backend.
-  // Exemplo: https://script.google.com/macros/s/AKfycb.../exec
-  API_URL: "",
+  API_URL: "https://script.google.com/macros/s/AKfycby2HN1DU9SXupv2WlthavWzXMr5k1RsdpzDL-Dahd9NN2-TvDvEqT-wqMM6F9fP2DnY/exec",
 
   FORM_PROTOCOLO_URL: "https://forms.gle/vQoChVX2mj61KZz27",
   FORM_RELATORIO_URL: "https://docs.google.com/forms/d/e/1FAIpQLSei77aFurJhdDZmmrOPZRAssw5OS9NOoT5xRGK8JwRMxxSlTg/viewform?usp=sharing&ouid=117860724975449383583",
