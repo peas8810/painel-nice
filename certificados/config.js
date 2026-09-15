c88a24feaf724331c5564aecb21984dfcf2fa342
PEAS - Technology <127704090+peas8810@users.noreply.github.com>
2026-09-15 15:26:21 -0300

    feat: adiciona configuração pública da emissão de certificados

---
+++ certificados/config.js
@@ -0,0 +1,4 @@
+window.NICE_CERT_CONFIG=Object.freeze({
+  API_URL:'https://script.google.com/macros/s/AKfycby2HN1DU9SXupv2WlthavWzXMr5k1RsdpzDL-Dahd9NN2-TvDvEqT-wqMM6F9fP2DnY/exec',
+  BASE_PATH:'/certificados/'
+});
